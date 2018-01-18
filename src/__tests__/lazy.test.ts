2318394a122a69063786509ae8a996243d2df882
Vadim Dalecky <streamich@gmail.com>
2018-01-18 15:48:03 +0000

    test: add tests for lazyIdle()

--- src/__tests__/lazy.test.ts
+++ src/__tests__/lazy.test.ts
@@ -1,4 +1,4 @@
-import {lazy} from '../lazy';
+import {lazy, lazyIdle} from '../lazy';
 import {Component, createElement as h} from 'react';
 import {shallow, mount} from 'enzyme';
 
@@ -64,6 +64,72 @@ describe('lazy', () => {
             reject(error);
           }
         });
+      });
+    });
+  });
+
+  describe('lazyIdle()', () => {
+    it('is a function', () => {
+      expect(typeof lazyIdle).toBe('function');
+    });
+
+    it('returns a stateless component', () => {
+      const Lazy = lazyIdle({
+        loader: () => Promise.resolve(() => null)
+      });
+
+      expect(Lazy).toBeInstanceOf(Function);
+    });
+
+    it('has .load() method', () => {
+      const Lazy = lazyIdle({
+        loader: () => Promise.resolve(() => null)
+      });
+
+      expect(Lazy.load).toBeInstanceOf(Function);
+    });
+
+    it('renders nothing by default', () => {
+      const Lazy = lazyIdle({
+        loader: () => Promise.resolve(() => null)
+      });
+      const wrapper = mount(h(Lazy));
+
+      expect(Boolean(wrapper.html())).toBe(false);
+    });
+
+    it('renders loading placeholder', () => {
+      const Lazy = lazyIdle({
+        loading: h('span', {}, 'Loading...'),
+        loader: () => Promise.resolve(() => null)
+      });
+      const wrapper = mount(h(Lazy));
+
+      expect(wrapper.find('span').html()).toBe('<span>Loading...</span>');
+    });
+
+    it('loads and renders actual implementation after first render', () => {
+      const Implementation = () => h('div', {}, 'IMPLEMENTATION');
+      const Lazy = lazyIdle({
+        loader: () => Promise.resolve(Implementation),
+        delay: 1
+      });
+
+      const wrapper = mount(h(Lazy));
+
+      expect(Boolean(wrapper.html())).toBe(false);
+
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          try {
+            wrapper.update();
+            expect(wrapper.find('div').html()).toBe('<div>IMPLEMENTATION</div>');
+
+            resolve();
+          } catch (error) {
+            reject(error);
+          }
+        }, 50);
       });
     });
   });
