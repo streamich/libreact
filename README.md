0d370222049528221df4fc57d00c4b574ca51d1b
Vadim Dalecky <streamich@gmail.com>
2018-01-18 15:31:30 +0000

    feat: expose .load() on Lazy components

--- README.md
+++ README.md
@@ -96,9 +96,13 @@ const LoadableComp = loadable({
 Creates a loadable component which is loaded when it is rendered for the first time.
 
 ```ts
-lazy: <TProps>(params: ILazyParams) => React.SFC<TProps>;
+lazy: <TProps>(params: ILazyParams) => ILazyComponent<TProps>;
 
 interface ILazyParams extends ILoadableParams {}
+
+export interface ILazyComponent<TProps> extends React.SFC<TProps> {
+    load();
+}
 ```
 
 Use it the same as `loadable()` function.
--- src/lazy.ts
+++ src/lazy.ts
@@ -5,20 +5,26 @@ export interface ILazyParams<TProps> extends ILoadableParams<TProps> {
 
 }
 
-export type TLazy = <TProps>(params: ILazyParams<TProps>) => React.SFC<TProps>;
+export interface ILazyComponent<TProps> extends React.SFC<TProps> {
+    load();
+}
+
+export type TLazy = <TProps>(params: ILazyParams<TProps>) => ILazyComponent<TProps>;
 
 export const lazy: TLazy = <TProps>(params) => {
     const Loadable = loadable<TProps>(params);
 
     let needsLoading = true;
-    const Lazy = (props: TProps) => {
+    const Lazy: ILazyComponent<TProps> = ((props: TProps) => {
         if (needsLoading) {
             needsLoading = false;
             Loadable.load();
         }
 
         return h(Loadable, props);
-    };
+    }) as ILazyComponent<TProps>;
+
+    Lazy.load = Loadable.load;
 
     return Lazy;
 };
