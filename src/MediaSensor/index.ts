daa3438bcb9189a346cbf2cc72a1afc9036b3101
Vadim Dalecky <streamich@gmail.com>
2018-01-19 18:12:54 +0000

    feat: add basic MediaSensor implementation

---
+++ src/MediaSensor/index.ts
@@ -0,0 +1,38 @@
+import {Component, createElement as h} from 'react';
+
+export interface IMediaSensorProps {
+  query: string;
+  children: (match: boolean) => React.ReactElement<any>;
+}
+
+export interface IMediaSensorState {
+  matches: boolean;
+}
+
+export class MediaSensor extends Component<IMediaSensorProps, IMediaSensorState> {
+  mql: MediaQueryList;
+
+  state: IMediaSensorState = {
+    matches: false
+  };
+
+  onMediaChange = (mediaQueryList) => {
+    this.setState({
+      matches: !!mediaQueryList.matches
+    });
+  };
+
+  updateQuery () {
+    const {query} = this.props;
+
+    this.mql = window.matchMedia(query);
+    this.mql.addListener(this.onMediaChange);
+  }
+
+  render () {
+    const {children} = this.props;
+    const {matches} = this.state;
+
+    return children(matches);
+  }
+}
