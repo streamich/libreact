d3c2f33d6fcc2902f6f97fb0180c41c18f2d083e
Vadim Dalecky <streamich@gmail.com>
2018-01-20 16:33:32 +0000

    feat: improve <Vibrate> story

---
+++ src/Vibrate/story.tsx
@@ -0,0 +1,23 @@
+import {Component, createElement as h} from 'react';
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
+import {Vibrate} from '.';
+
+storiesOf('Vibrate', module)
+  .add('single', () =>
+    <div>
+      <Vibrate ms={200} />
+      <pre style={{fontFamily: 'monospace'}}>
+        {'<Vibrate ms={200} />'}
+      </pre>
+    </div>
+  )
+  .add('sequence', () =>
+    <div>
+      <Vibrate ms={[100, 100, 100, 100, 100]} />
+      <pre style={{fontFamily: 'monospace'}}>
+        {'<Vibrate ms={[100, 100, 100, 100, 100]} />'}
+      </pre>
+    </div>
+  );
