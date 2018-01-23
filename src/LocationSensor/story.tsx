254c772c6d42585b83e37499f3d7c7b0d543d972
Vadim Dalecky <streamich@gmail.com>
2018-01-23 18:04:35 +0000

    feat: add <LocationSensor> story

--- src/LocationSensor/story.tsx
+++ src/LocationSensor/story.tsx
@@ -7,16 +7,24 @@ import ShowDocs from '../../.storybook/ShowDocs'
 
 class StoryLocationSensroExample extends Component<any, any> {
   state = {
-    url: 'page.html'
+    url: 'page.html',
+    state: 'STATE'
   };
 
   render () {
     return (
       <div>
         <input value={this.state.url} onChange={(e) => this.setState({url: e.target.value})} />
+        <input value={this.state.state} onChange={(e) => this.setState({state: e.target.value})} />
         <button onClick={() => {
-          history.pushState({}, '', this.state.url);
+          history.pushState(this.state.state, '', this.state.url);
         }}>Push state</button>
+
+        <br />
+
+        <button onClick={() => history.go(-1)}>history.back()</button>
+        <button onClick={() => history.go(1)}>history.forward()</button>
+
         <LocationSensor>{(state) =>
           <pre style={{fontFamily: 'monospace'}}>
             {JSON.stringify(state, null, 4)}
