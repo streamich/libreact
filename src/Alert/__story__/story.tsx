20b3dc299e83d82e2078660d59270a0d9209a7cc
Vadim Dalecky <streamich@gmail.com>
2018-01-24 23:56:38 +0000

    test: add <Alert> story

---
+++ src/Alert/__story__/story.tsx
@@ -0,0 +1,11 @@
+import {createElement as h} from 'react';
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
+import {Alert} from '..';
+import ShowDocs from '../../../.storybook/ShowDocs'
+
+storiesOf('Generators/Alert', module)
+  .add('Documentation', () => h(ShowDocs, {name: 'Alert'}))
+  .add('Basic example', () => <Alert show text='Hello world' />)
+  .add('Don\'t show', () => <Alert show={false} text='You shall not see this' />);
