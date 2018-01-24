7ad33b18aafa2329c2c1cccc91cf24fe79be6cdd
Vadim Dalecky <streamich@gmail.com>
2018-01-24 23:52:49 +0000

    feat: add <Alert> component

---
+++ src/Alert/index.ts
@@ -0,0 +1,29 @@
+import {PureComponent} from 'react';
+import {noop, isClient} from '../util';
+
+export interface IAlertProps {
+  show?: boolean;
+  text?: string;
+}
+
+export class Alert extends PureComponent<IAlertProps, any> {
+  componentDidMount () {
+    this.alert();
+  }
+
+  componentDidUpdate () {
+    this.alert();
+  }
+
+  alert () {
+    const {show, text} = this.props;
+
+    if (show) {
+      alert(text);
+    }
+  }
+
+  render () {
+    return null;
+  }
+}
