8e6a46b723a8b7a999b97d114ac56540eec0cb51
Vadim Dalecky <streamich@gmail.com>
2018-01-27 22:37:59 +0000

    fix: change internals of <Map>
    Use a key of React components state rather than the whole state object
    for the map, because .replaceState() method was removed from React and
    there is no good way to remove properties from map using native state.

--- src/Map/index.ts
+++ src/Map/index.ts
@@ -1,27 +1,27 @@
 import {h} from '../util';
 import {State} from '../State';
 import renderProp from '../util/renderProp';
-import faccToHoc from '../util/faccToHoc';
+import {Value, faccToHocInit} from '../Value';
 
 export interface IMapProps {
   init?: {[key: string]: any};
 }
 
 export const Map: React.StatelessComponent<IMapProps> = (props) => {
-  return h(State, {
-    init: props.init && (typeof props.init === 'object') ? props.init : [],
-    render: (state, set) => renderProp(props, {
-      get: (key) => state[key],
-      set: (key, value) => set({
-        ...state,
-        [key]: value
+  return Value({
+    init: props.init && (typeof props.init === 'object') ? props.init : {},
+    render: ({value, set}) => renderProp(props, {
+      get: (key?) => key ? value[key] : value,
+      set: (key, entry) => set({
+        ...value,
+        [key]: entry
       }),
       remove: (key) => {
-        const {[key]: omit, ...rest} = state;
+        const {[key]: omit, ...rest} = value;
         set(rest);
       }
     })
   });
 };
 
-export const withMap = faccToHoc(Map, 'map');
+export const withMap = faccToHocInit(Map, 'map');
