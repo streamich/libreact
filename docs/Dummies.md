aae9ccb63fdfff9b9e1265c28867ac7f1ff17d0c
Vadim Dalecky <streamich@gmail.com>
2018-01-28 01:25:51 +0000

    feat: add <MouseSensor> component

--- docs/Dummies.md
+++ docs/Dummies.md
@@ -6,11 +6,11 @@ However, those dummies can be used as real React components and they will re-ren
 automatically once they get implemented.
 
 Essentially you can create dummies, which will not add any size to your bundle, use them,
-but implement them with real component only when necessary.
+but implement them with real components only later when necessary.
 
   - [`mock()`](./mock.md) - dummy that can be implemented using `.implement()` method.
   - [`loadable()`](./loadable.md) - dummy that can be loaded using `.load()` method.
   - [`lazy()`](./lazy.md) - dummy that loads automatically when rendered for the first time.
   - [`delayed()`](./delayed.md) - like `lazy()`, but its loading can be delayed further.
-  - [`viewport()`](./viewport.md) - dummy that is loaded automatically when it appears for
-  the first time in viewport.
+  - [`viewport()`](./viewport.md) - postpones component's rendering until it is visible
+  in viewport at least once.
---
+++ src/MouseSensor/index.ts
@@ -0,0 +1,127 @@
+import {Component, Children, cloneElement} from 'react';
+import {h, noop} from '../util';
+import renderProp from '../util/renderProp';
+import {extend} from 'fast-extend';
+
+export interface IMouseSensorProps {
+  children?: (state: IMouseSensorState) => React.ReactElement<any>;
+  render?: (state: IMouseSensorState) => React.ReactElement<any>;
+  onMouse,
+  onMouseEnter,
+  onMouseLeave
+  whenHovered
+}
+
+export interface IMouseSensorState {
+  docX: number;
+  docY: number;
+  posX: number;
+  posY: number;
+  elH: number;
+  elW: number;
+  elX: number;
+  elY: number;
+}
+
+export class MouseSensor extends Component<IMouseSensorProps, IMouseSensorState> {
+  static defaultProps = {
+    onMouseEnter: noop,
+    onMouseLeave: noop,
+    whenHovered: true
+  };
+
+  state: IMouseSensorState = {
+    docX: 0,
+    docY: 0,
+    posX: 0,
+    posY: 0,
+    elH: 0,
+    elW: 0,
+    elX: 0,
+    elY: 0
+  };
+
+  el: HTMLElement = null;
+  frame = null;
+
+  ref = (originalRef) => (el) => {
+    this.el = el;
+    (originalRef || noop)(el);
+  };
+
+  componentDidMount () {
+    if (!this.props.whenHovered) {
+      this.bindEvents();
+    }
+  }
+
+  componentWillUnmount () {
+    this.unbindEvents();
+  }
+
+  bindEvents () {
+    document.addEventListener('mousemove', this.onDocMouseMove);
+  }
+
+  unbindEvents () {
+    document.removeEventListener('mousemove', this.onDocMouseMove);
+  }
+
+  onDocMouseMove = (event) => {
+    const {el} = this;
+
+    if (!el) {
+      return;
+    }
+
+    cancelAnimationFrame(this.frame);
+    this.frame = requestAnimationFrame(() => {
+      const {left, top} = el.getBoundingClientRect();
+      const posX = left + window.scrollX;
+      const posY = top + window.scrollY;
+
+      this.setState({
+        docX: event.pageX,
+        docY: event.pageY,
+        posX,
+        posY,
+        elH: el.offsetHeight,
+        elW: el.offsetWidth,
+        elX: event.pageX - posX,
+        elY: event.pageY - posY
+      });
+    });
+  };
+
+  onMouseEnter = (originalOnMouseMove) => (event) => {
+    if (this.props.whenHovered) {
+      this.bindEvents();
+    }
+    (originalOnMouseMove || noop)(event);
+  };
+
+  onMouseLeave = (originalOnMouseMove) => (event) => {
+    if (this.props.whenHovered) {
+      this.unbindEvents();
+    }
+    (originalOnMouseMove || noop)(event);
+  };
+
+  render () {
+    const element = renderProp(this.props, this.state);
+
+    let newProps: any = {
+      ...element.props,
+      ref: this.ref(element.ref)
+    };
+
+    if (this.props.whenHovered) {
+      const {onMouseEnter, onMouseLeave} = element.props;
+
+      newProps.onMouseEnter = this.onMouseEnter(onMouseEnter);
+      newProps.onMouseLeave = this.onMouseLeave(onMouseLeave);
+    }
+
+    return cloneElement(element, newProps);
+  }
+}
