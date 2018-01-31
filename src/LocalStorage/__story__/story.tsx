2ee63a75c980def4550a2dc5c8512b9610d3c90a
Vadim Dalecky <streamich@gmail.com>
2018-01-31 19:58:23 +0000

    fix: localStorage can throw, wrap in try block

--- src/LocalStorage/__story__/story.tsx
+++ src/LocalStorage/__story__/story.tsx
@@ -2,9 +2,9 @@ import {createElement as h} from 'react';
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
 import {linkTo} from '@storybook/addon-links';
-import {LocalStorage} from '.';
-import StoryLocalStorageForm from './stories/StoryLocalStorageForm';
-import ShowDocs from '../../.storybook/ShowDocs'
+import {LocalStorage} from '..';
+import StoryLocalStorageForm from './StoryLocalStorageForm';
+import ShowDocs from '../../../.storybook/ShowDocs'
 
 storiesOf('Generators/LocalStorage', module)
   .add('Documentation', () => h(ShowDocs, {name: 'LocalStorage'}))
