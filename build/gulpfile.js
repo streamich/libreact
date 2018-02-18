0ea10f3aef0e419c9752c34f4ff752c8af9e1485
Vadim Dalecky <streamich@gmail.com>
2018-02-18 02:00:08 +0000

    fix: fix gulpfile build paths

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,33 +1,34 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const tsConfig = require('../tsconfig');
+const path = require('path');
 
 gulp.task('build-ts', () => {
     return gulp.src([
-      'src/**/*.ts',
-      '!src/**/__tests__/**',
-      '!src/**/__story__/**',
-      '!src/**/__docs__/**',
-      '!src/**/story.ts',
-      '!src/**/story.tsx'
+      '../src/**/*.ts',
+      '!../src/**/__tests__/**',
+      '!../src/**/__story__/**',
+      '!../src/**/__docs__/**',
+      '!../src/**/story.ts',
+      '!../src/**/story.tsx'
     ]).pipe(ts({
       ...tsConfig.compilerOptions,
       target: 'es5',
       module: 'commonjs'
-    })).pipe(gulp.dest('lib'));
+    })).pipe(gulp.dest('../lib'));
 });
 
 gulp.task('build-modules', () => {
   return gulp.src([
-    'src/**/*.ts',
-    '!src/**/__tests__/**',
-    '!src/**/__story__/**',
-    '!src/**/__docs__/**',
-    '!src/**/story.ts',
-    '!src/**/story.tsx'
+    '../src/**/*.ts',
+    '!../src/**/__tests__/**',
+    '!../src/**/__story__/**',
+    '!../src/**/__docs__/**',
+    '!../src/**/story.ts',
+    '!../src/**/story.tsx'
   ]).pipe(ts({
     ...tsConfig.compilerOptions,
     target: 'ESNext',
     module: 'ESNext'
-  })).pipe(gulp.dest('modules'));
+  })).pipe(gulp.dest('../modules'));
 });
