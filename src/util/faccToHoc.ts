472b2402637ee8d143bfaec39bd7bc31907886e3
Vadim Dalecky <streamich@gmail.com>
2018-02-20 00:47:45 +0000

    refactor: use react-universal-interface

--- src/util/faccToHoc.ts
+++ src/util/faccToHoc.ts
@@ -1,29 +1,7 @@
-import {h} from '../util';
-import addClassDecoratorSupport from './addClassDecoratorSupport';
+import createEnhancer, {divWrapper} from 'react-universal-interface/lib/createEnhancer';
 
-const noWrap = (Comp, propName, props, state) => h(Comp, propName ?
-  {[propName]: state, ...props} :
-  {...state, ...props}
-);
+export {
+  divWrapper
+};
 
-export const divWrapper = (Comp, propName, props, state) =>
-  h('div', null, noWrap(Comp, propName, props, state)) as any;
-
-const faccToHoc = (Facc, prop?: string, wrapper = noWrap) => {
-  const hoc = (Comp, propName: any = prop, faccProps: object = null) => {
-    const isClassDecoratorMethodCall = typeof Comp === 'string';
-
-    if (isClassDecoratorMethodCall) {
-      return (Klass) => hoc(Klass, Comp as any, propName as any);
-    }
-
-    const Enhanced = (props) =>
-      h(Facc, faccProps, (state) => wrapper(Comp, propName, props, state));
-
-    return isClassDecoratorMethodCall ? addClassDecoratorSupport(Enhanced) : Enhanced;
-  };
-
-  return hoc;
-}
-
-export default faccToHoc;
+export default createEnhancer;
--- src/util/renderProp.ts
+++ src/util/renderProp.ts
@@ -1,33 +1,3 @@
-import {h, isFn} from '../util';
+import {render} from 'react-universal-interface';
 
-const renderProp = (props, ...args) => {
-  if (process.env.NODE_ENV !== 'production') {
-    if (typeof props !== 'object') {
-      throw new TypeError('renderProp(props, data) first argument must be a prop object.');
-    }
-
-    const {children, render} = props;
-
-    if (isFn(children) && isFn(render)) {
-      console.warn(
-        'Both "render" and "children" are specified for a render-prop component. ' +
-        'Children will be used.'
-      );
-      console.trace();
-    }
-  }
-
-  const {render, children = render, component, comp = component} = props;
-
-  return isFn(children) ?
-    children(...args) :
-    comp ?
-      h(comp, args[0]) :
-      children && (typeof children !== 'object') ?
-        h('div', null, children) :
-        children instanceof Array ?
-          h('div', null, ...children) :
-          (children || null);
-};
-
-export default renderProp;
+export default render;
