30b0e8496002cebe517dec763769ddb04649e054
Vadim Dalecky <streamich@gmail.com>
2018-02-23 17:48:10 +0000

    feat: add <AfterTimeout> component

---
+++ src/AfterTimeout/index.ts
@@ -0,0 +1,34 @@
+import {Component} from 'react';
+import {render} from 'react-universal-interface';
+
+export interface IAfterTimeoutProps {
+  ms: number;
+}
+
+export interface IAfterTimeoutState {
+  ready: boolean;
+}
+
+export class AfterTimeout extends Component<IAfterTimeoutProps, IAfterTimeoutState> {
+  state: IAfterTimeoutState = {
+    ready: false
+  };
+
+  timer;
+
+  componentDidMount () {
+    this.timer = setTimeout(() => {
+      this.setState({
+        ready: true
+      });
+    }, this.props.ms);
+  }
+
+  componentWillUnmount () {
+    clearTimeout(this.timer);
+  }
+
+  render () {
+    return this.state.ready ? render(this.props, {}) : null;
+  }
+}
