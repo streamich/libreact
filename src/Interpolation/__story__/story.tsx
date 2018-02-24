160f5a1bfe6b9fab46b20f263584900ce36e70dd
Vadim Dalecky <streamich@gmail.com>
2018-02-24 00:53:42 +0000

    test: add <Interpolation> story

--- src/Interpolation/__story__/story.tsx
+++ src/Interpolation/__story__/story.tsx
@@ -14,3 +14,22 @@ storiesOf('Animation/Interpolation', module)
       }</Interpolation>
     </div>
   )
+  .add('Basic map', () =>
+    <div>
+        <Interpolation ms={1000} easing='inQuint' map={{
+          left: [120, 300],
+          top: [22, 322],
+          opacity: [0, 1]
+        }}>{({left, top, opacity}) =>
+          <div style={{
+            width: 100,
+            height: 100,
+            background: 'tomato',
+            opacity,
+            position: 'relative',
+            top,
+            left
+          }}/>
+        }</Interpolation>
+    </div>
+  )
