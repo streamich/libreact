8aad3d7dd61b1093732d62700b3c3ec9952b4295
Vadim Dalecky <streamich@gmail.com>
2018-02-24 17:36:48 +0000

    feat: add <DropArea> component

---
+++ src/DropArea/index.ts
@@ -0,0 +1,87 @@
+import {Component, cloneElement} from 'react';
+import {render} from 'react-universal-interface';
+import {h, noop} from '../util';
+
+const REG_URL = /^https?:\/\//;
+
+export interface IDropAreaProps {
+  onFiles?: (files: File[], event?) => void;
+  onText?: (text: string, event?) => void;
+  onUri?: (url: string, event?) => void;
+}
+
+export interface IDropAreaState {
+}
+
+export class DropArea extends Component<IDropAreaProps, IDropAreaState> {
+  static defaultProps = {
+    onFiles: noop,
+    onText: noop,
+    onUrl: noop,
+  };
+
+  mounted = true;
+
+  coponentWillUnmount () {
+    this.mounted = false;
+  }
+
+  onDragOver = (originalDragOver) => (event) => {
+    (originalDragOver || noop)(event);
+    event.preventDefault();
+  };
+
+  onDragEnter = (originalDragEnter) => (event) => {
+    (originalDragEnter || noop)(event);
+    event.preventDefault();
+  };
+
+  onDrop = (originalDrop) => (event) => {
+    (originalDrop || noop)(event);
+    event.preventDefault();
+
+    this.process(event.dataTransfer, event);
+    const {dataTransfer} = event;
+  };
+
+  onPaste = (originalOnPaste) => (event) => {
+    (originalOnPaste || noop)(event);
+
+    this.process(event.clipboardData, event);
+  };
+
+  process (dataTransfer: DataTransfer, event) {
+    const uri = dataTransfer.getData('text/uri-list');
+
+    if (uri) {
+      this.props.onUri(uri, event);
+      return;
+    }
+
+    if (dataTransfer.files && dataTransfer.files.length) {
+      this.props.onFiles(Array.from(dataTransfer.files), event);
+      return;
+    }
+
+    if (dataTransfer.items && dataTransfer.items.length) {
+      dataTransfer.items[0].getAsString((text) => {
+        if (this.mounted) {
+          this.props.onText(text);
+        }
+      });
+    }
+  }
+
+  render () {
+    const element = render(this.props, {});
+    const {props} = element;
+
+    return cloneElement(element, {
+      ...props,
+      onDragOver: this.onDragOver(props.onDragOver),
+      onDragEnter: this.onDragEnter(props.onDragEnter),
+      onDrop: this.onDrop(props.onDrop),
+      onPaste: this.onPaste(props.onPaste),
+    });
+  }
+}
