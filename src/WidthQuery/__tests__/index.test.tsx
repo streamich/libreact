72510d30ff61f95cc21241174c4e81a2f20317b0
Vadim Dalecky <streamich@gmail.com>
2018-03-04 02:18:51 +0000

    test: add <WidthQuery> tests

---
+++ src/WidthQuery/__tests__/index.test.tsx
@@ -0,0 +1,71 @@
+import {createElement as h} from 'react';
+import {render} from 'react-dom';
+import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
+import {WidthQuery} from '..';
+import {View} from '../../View';
+
+describe('<ViewQuery/>', () => {
+  it('renders with crashing, expects multiple <View/> children', () => {
+    expect(() => {
+      render(<WidthQuery width={Infinity} />, document.createElement('div'));
+    }).toThrowErrorMatchingSnapshot();
+  });
+
+  it('crashes if less than two <View/> children provided', () => {
+    expect(() => {
+      const jsx = <WidthQuery width={100}>
+        <View maxWidth={123}>view 1</View>
+      </WidthQuery>;
+
+      render(jsx, document.createElement('div'));
+    }).toThrowErrorMatchingSnapshot();
+  });
+
+  it('renders first matching view', () => {
+    const jsx = <WidthQuery width={100}>
+      <View maxWidth={123}>view 1</View>
+      <View maxWidth={123}>view 2</View>
+    </WidthQuery>;
+    const wrapper = shallow(jsx);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('picks the correct <View/> by maxWidth 1', () => {
+    const jsx = <WidthQuery width={700}>
+      <View maxWidth={200} />
+      <View maxWidth={400}>view 2</View>
+      <View maxWidth={600}>view 3</View>
+      <View maxWidth={800}>view 4</View>
+      <View maxWidth={1000}>view 5</View>
+    </WidthQuery>;
+    const wrapper = shallow(jsx);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('picks the correct <View/> by maxWidth 2', () => {
+    const jsx = <WidthQuery width={700}>
+      <View maxWidth={600}>view 2</View>
+      <View maxWidth={700}>view 3</View>
+      <View maxWidth={800}>view 4</View>
+      <View maxWidth={900}>view 5</View>
+    </WidthQuery>;
+    const wrapper = shallow(jsx);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('treats empty maxWidth as Infinity', () => {
+    const jsx = <WidthQuery width={2000}>
+      <View maxWidth={600}>view 2</View>
+      <View maxWidth={700}>view 3</View>
+      <View maxWidth={800}>view 4</View>
+      <View>view 5</View>
+    </WidthQuery>;
+    const wrapper = shallow(jsx);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
