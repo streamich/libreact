9ea1e6fda35c196eabc14ab10fa8c21dee486f99
Vadim Dalecky <streamich@gmail.com>
2018-03-23 11:03:56 +0000

    test: add loadable() story

--- src/mock.ts
+++ src/mock.ts
@@ -32,7 +32,7 @@ export const mock: TMock = <TProps>({loading = null}: IMockParams<TProps> = {}) 
     }
 
     render () {
-      return Comp ? h(Comp, this.props) : (typeof loading === 'function' ? h(loading, this.props) : loading);
+      return Comp ? h(Comp, this.props) : (typeof loading === 'function' ? h(loading as any, this.props) : loading);
     }
   } as any as IMockComponent<TProps>;
 
