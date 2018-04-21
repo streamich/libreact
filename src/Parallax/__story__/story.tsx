9a80433f4c9b6cc641d6686e18039699216ae831
Vadim Dalecky <streamich@gmail.com>
2018-04-21 10:35:25 +0100

    test: add <Parallax> story with margins

--- src/Parallax/__story__/story.tsx
+++ src/Parallax/__story__/story.tsx
@@ -2,7 +2,9 @@ import {createElement as h} from 'react';
 import {storiesOf} from '@storybook/react';
 import StoryParallax1 from './StoryParallax1';
 import StoryParallax2 from './StoryParallax2';
+import StoryParallax3 from './StoryParallax3';
 
 storiesOf('UI/Parallax', module)
   .add('Basic example', () => <StoryParallax1 />)
-  .add('Card', () => <StoryParallax2 />);
+  .add('Card', () => <StoryParallax2 />)
+  .add('With margin', () => <StoryParallax3 />);
