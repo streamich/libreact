6314ac620cedc82f9372aad54f4251383d43d599
Vadim Dalecky <streamich@gmail.com>
2018-04-21 09:19:29 +0100

    feat: make basic version of <Parallax> work

---
+++ src/Parallax/__story__/StoryParallax1.tsx
@@ -0,0 +1,26 @@
+import {Parallax} from '..';
+import {h} from '../../util';
+
+const StoryParallax1 = ({onChange}) => {
+  return (
+    <div>
+      <div style={{
+        width: 300,
+        height: 1e3,
+        border: '1px solid tomato'
+      }} />
+
+      <Parallax threshold={1} onChange={onChange}>{(state) =>
+        <pre>{JSON.stringify(state, null, 4)}</pre>
+      }</Parallax>
+
+      <div style={{
+        width: 300,
+        height: 1e3,
+        border: '1px solid tomato'
+      }} />
+    </div>
+  );
+};
+
+export default StoryParallax1;
--- src/Parallax/index.ts
+++ src/Parallax/index.ts
@@ -8,14 +8,25 @@ export interface IParallaxState extends IViewportScrollSensorState {
 }
 
 export class Parallax extends ViewportScrollSensor<IParallaxProps, IParallaxState> {
-  onCalculation (visible, rectRoot: TRect, rectEl: TRect, rectIntersection: TRect) {
-    if (visible !== this.state.visible) {
-      const state = {
-        visible
-      };
+  update (visible, rectRoot: TRect, rectEl: TRect, rectIntersection: TRect) {
+    const state = {
+      visible,
+      rectRoot,
+      rectEl,
+      rectIntersection,
+    };
 
-      this.setState(state);
-      (this.props.onChange || noop)(state);
+    this.setState(state);
+    (this.props.onChange || noop)(state);
+  }
+
+  onCalculation (visible, rectRoot: TRect, rectEl: TRect, rectIntersection: TRect) {
+    if (visible) {
+      this.update(visible, rectRoot, rectEl, rectIntersection);
+    } else {
+      if (this.state.visible) {
+        this.update(visible, rectRoot, rectEl, rectIntersection);
+      }
     }
   }
 }
