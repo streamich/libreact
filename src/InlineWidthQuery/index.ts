bcecf6229850c77854cbfc3c12090640858a4e0a
Vadim Dalecky <streamich@gmail.com>
2018-05-01 23:45:31 +0100

    feat: export <View> from query component modules

--- src/InlineWidthQuery/index.ts
+++ src/InlineWidthQuery/index.ts
@@ -2,6 +2,8 @@ import {h} from '../util';
 import {WidthQuery} from '../WidthQuery';
 import {SizeSensor} from '../SizeSensor';
 
+export {View} from '../View';
+
 export const InlineWidthQuery = (props) =>
   h(SizeSensor, null, (state) =>
     h('div', null,
--- src/WindowWidthQuery/index.ts
+++ src/WindowWidthQuery/index.ts
@@ -2,6 +2,8 @@ import {h} from '../util';
 import {WidthQuery} from '../WidthQuery';
 import {WindowSizeSensor} from '../WindowSizeSensor';
 
+export {View} from '../View';
+
 export const WindowWidthQuery = (props) =>
   h(WindowSizeSensor, null,
     (state) => h(WidthQuery, state, props.children)
