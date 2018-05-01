f2e60663f02898e64dabd1b71c7adf86df9994c2
Vadim Dalecky <streamich@gmail.com>
2018-05-01 23:40:07 +0100

    fix: .tsx -> .ts so <WindowWidthQuery> is in build

---
+++ src/WindowWidthQuery/index.ts
@@ -0,0 +1,8 @@
+import {h} from '../util';
+import {WidthQuery} from '../WidthQuery';
+import {WindowSizeSensor} from '../WindowSizeSensor';
+
+export const WindowWidthQuery = (props) =>
+  h(WindowSizeSensor, null,
+    (state) => h(WidthQuery, state, props.children)
+  );
--- src/WindowWidthQuery/index.tsx
+++
@@ -1,5 +0,0 @@
-import {h} from '../util';
-import {WidthQuery} from '../WidthQuery';
-import {WindowSizeSensor} from '../WindowSizeSensor';
-
-export const WindowWidthQuery = (props) => h(WindowSizeSensor, null, (state) => h(WidthQuery, state, props.children));
