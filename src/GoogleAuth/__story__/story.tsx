ece36764737cfbf45a4c1a8bdf74f816801719cc
streamich <streamich@gmail.com>
2018-09-30 17:30:36 +0200

    feat: 🎸 implement Google auth Provider and Consumer components

--- src/GoogleAuth/__story__/story.tsx
+++ src/GoogleAuth/__story__/story.tsx
@@ -4,14 +4,34 @@ import {createGoogleAuthContext} from '..';
 // import ShowDocs from '../../../.storybook/ShowDocs'
 
 const clientId = '305188012168-htfit0k0u4vegn0f6hn10rcqoj1m77ca.apps.googleusercontent.com';
-const ctx1 = createGoogleAuthContext(clientId);
+const options = {
+  client_id: clientId,
+};
+const ctx1 = createGoogleAuthContext(options);
 
 storiesOf('Context/GoogleAuthButton', module)
   // .add('Documentation', () => h(ShowDocs, {md: require('../../../docs/en/GoogleAuthButton.md')}))
   .add('Default', () =>
     <ctx1.Provider>
-      <ctx1.Consumer>{({signIn}) =>
-        <button onClick={signIn}>Sign in with Google!</button>
-      }</ctx1.Consumer>
+      <ctx1.Consumer>{({loading, signIn, signOut, isSignedIn, user}) => {
+        if (loading) {
+          return 'Loading...';
+        }
+        console.log('user', user);
+        return (
+          <div>
+            <button onClick={isSignedIn ? signOut : signIn}>
+              {isSignedIn ? 'Log out' : 'Sign in with Google!'}
+            </button>
+            <div>Is signed in: {isSignedIn ? 'true' : 'false'}</div>
+            {user &&
+              <div>
+                <div>Name: {user.getBasicProfile().getName()}</div>
+                <div>JWT: {user.getAuthResponse().id_token}</div>
+              </div>
+            }
+          </div>
+        );
+      }}</ctx1.Consumer>
     </ctx1.Provider>
   );
--- src/GoogleAuth/gapi.ts
+++ src/GoogleAuth/gapi.ts
@@ -1,58 +1,107 @@
-export type GoogleAPI = any;
+export interface GApi {
+  load(what: 'auth2', callback: () => void);
+  auth2: GApiAuth2;
+}
 
-let gapiCache: GoogleAPI;
+export interface GApiAuth2InitOptions {
+  client_id: string;
+  scope?: string;
+}
 
-export const getGapi = async (): Promise<GoogleAPI> => {
-    if (gapiCache) {
-        return gapiCache;
-    }
+export interface GApiAuth2 {
+  init(options: GApiAuth2InitOptions): Promise<GApiAuth2Instance>;
+}
 
-    await new Promise(resolve => {
-        const gapicallback = `__gapicb${Date.now().toString(36)}`;
-        (window as any)[gapicallback] = () => {
-            delete (window as any)[gapicallback];
-            gapiCache = (window as any).gapi;
-            resolve();
-        };
+export type GApiAuth2InstanceIsSignedInListener = (isSignedIn: boolean) => void;
+export type GApiAuth2InstanceCurrentUserListener = (isSignedIn: GApiAuth2User) => void;
+export interface GApiAuth2Instance {
+  signIn(): Promise<GApiAuth2User>;
+  signOut(): Promise<void>;
+  isSignedIn: {
+    get(): boolean;
+    listen(listener: GApiAuth2InstanceIsSignedInListener);
+  };
+  currentUser: {
+    get(): GApiAuth2User;
+    listen(listener: GApiAuth2InstanceCurrentUserListener);
+  };
+}
 
-        const script = document.createElement('script');
+export interface GApiAuth2User {
+  getId(): string;
+  isSignedIn(): boolean;
+  getHostedDomain(): string;
+  getGrantedScopes(): string;
+  getBasicProfile(): GApiAuth2BasicProfile;
+  getAuthResponse(): GApiAuth2AuthResponse;
+  reloadAuthResponse(): Promise<GApiAuth2AuthResponse>;
+  hasGrantedScopes(scopes: string): boolean;
+}
 
-        script.src = 'https://apis.google.com/js/platform.js?onload=' + gapicallback;
-        document.head.appendChild(script);
-    });
+export interface GApiAuth2BasicProfile {
+  getId(): string;
+  getName(): string;
+  getGivenName(): string;
+  getFamilyName(): string;
+  getImageUrl(): string;
+  getEmail(): string;
+}
 
+export interface GApiAuth2AuthResponse {
+  access_token: string;
+  id_token: string;
+  scope: string;
+  expires_in: string;
+  first_issued_at: string;
+  expires_at: string;
+}
+
+let gapiCache: GApi;
+
+export const getGapi = async (): Promise<GApi> => {
+  if (gapiCache) {
     return gapiCache;
+  }
+
+  await new Promise(resolve => {
+    const gapicallback = `__gapicb${Date.now().toString(36)}`;
+    (window as any)[gapicallback] = () => {
+      delete (window as any)[gapicallback];
+      gapiCache = (window as any).gapi;
+      resolve();
+    };
+
+    const script = document.createElement('script');
+
+    script.src = 'https://apis.google.com/js/platform.js?onload=' + gapicallback;
+    document.head.appendChild(script);
+  });
+
+  return gapiCache;
 };
 
 let gapiAuth2Cache;
 
-export const getGapiAuth2 = async (): Promise<any> => {
-    if (gapiAuth2Cache) {
-        return gapiAuth2Cache;
-    }
-
-    const gapi = await getGapi();
-
-    await new Promise(resolve => {
-        gapi.load('auth2', () => {
-            gapiAuth2Cache = gapi.auth2;
-            resolve();
-        });
-    });
-
+export const getGapiAuth2 = async (): Promise<GApiAuth2> => {
+  if (gapiAuth2Cache) {
     return gapiAuth2Cache;
+  }
+
+  const gapi = await getGapi();
+
+  await new Promise(resolve => {
+    gapi.load('auth2', () => {
+      gapiAuth2Cache = gapi.auth2;
+      resolve();
+    });
+  });
+
+  return gapiAuth2Cache;
 };
 
-let GoogleAuthCache;
+export const getGapiAuthInstance = async (options: GApiAuth2InitOptions) => {
+  const gapiAuth2 = await getGapiAuth2();
+  const GoogleAuthCache = await gapiAuth2.init(options);
 
-export const getGapiAuthInstance = async (client_id: string) => {
-    if (GoogleAuthCache) {
-        return GoogleAuthCache;
-    }
-
-    const gapiAuth2 = await getGapiAuth2();
-
-    GoogleAuthCache = await gapiAuth2.init({client_id});
-
-    return GoogleAuthCache;
+  return GoogleAuthCache;
 };
