d8748b90f186c9c0be9e6bc81e2496f675826479
Va Da <streamich@users.noreply.github.com>
2018-12-22 18:51:51 +0100

    fix: don't coerce to false

--- src/Lifecycles/index.ts
+++ src/Lifecycles/index.ts
@@ -26,8 +26,8 @@ export class Lifecycles extends Component<ILifecyclesProps, {}> {
   }
 
   shouldComponentUpdate (nextProps) {
-    const fn = this.props.shouldUpdate;
-    return fn ? Boolean(fn(nextProps, this.props)) : true;
+    const result = this.props.shouldUpdate(nextProps, this.props);
+    return typeof result === 'boolean' ? result : true;
   }
 
   getSnapshotBeforeUpdate(prevProps) {
